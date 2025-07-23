75e1b5182169465cc8fb30ded76f1f7451ca7237
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2025-07-23 07:26:51 +0300

    Update faceRecognitionRoutes.js

--- routes/faceRecognitionRoutes.js
+++ routes/faceRecognitionRoutes.js
@@ -1,27 +1,27 @@
 // routes/faceRecognitionRoutes.js
-// const express = require("express");
+const express = require("express");
 
-// const router = express.Router();
-// const axios = require("axios");
-// // const FormData = require("form-data");
+const router = express.Router();
+const axios = require("axios");
+// const FormData = require("form-data");
 
-// router.post("/recognize", async (req, res) => {
-//   try {
-//     const response = await axios.post(
-//       "http://192.168.1.10:5000/recognize",
-//       req.body,
-//       {
-//         headers: {
-//           ...req.headers,
-//           "content-type": "multipart/form-data"
-//         }
-//       }
-//     );
-//     res.json(response.data);
-//   } catch (error) {
-//     res.status(500).json({ error: "Face recognition service failed" });
-//   }
-// });
+router.post("/recognize", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "http://192.168.1.10:5000/recognize",
+      req.body,
+      {
+        headers: {
+          ...req.headers,
+          "content-type": "multipart/form-data"
+        }
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Face recognition service failed" });
+  }
+});
 
 // module.exports = router;
 // const express = require("express");
