77c4b5f61364d59598285c033732e704b3e71f02
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2025-07-23 07:07:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ cloudinary.v2.config({
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://127.0.0.1:3000",
       process.env.FRONTEND_URL,
       process.env.DASHBOARD_URL,
       // "https://attendance-system-mu.vercel.app",
